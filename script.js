72b919e7d851bbf4b28cbf8ee0526fa7dbeb9f9f
Ihsan <neoglanding@protonmail.com>
2021-08-10 13:03:52 +0700

    modal berhasil

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-// API Key 2eb8dae5e1988df3f72f80c01551e09a
+// API KEY 2eb8dae5e1988df3f72f80c01551e09a
+
+function closeModal() {
+    document.getElementsByClassName('modal__film')[0].classList.add('hidden');
+}
+function showModal(data) {
+    const prod = data.production_companies;
+    // console.log(prod)
+    // console.log(prod.forEach(el => console.log(el.name)))
+    const genre = data.genres;
+    // console.log(genre)
+    const html = `<div class="myModal">
+            <div class="modal-content">
+                <div onclick="closeModal();" class="relative close__button mb-4 w-8 h-8 bg-red-300 rounded-full float-right hover:shadow-2xl">
+                    <p class="pos-cent text-white">X</p>
+                </div>
+                <div class="info flex mt-4 flex-wrap justify-center gap-x-16">
+                    <img class="w-96"src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${data.poster_path}" alt="">
+                    <div class="info__film w-96">
+                        <h1 class="text-xl font-bold">${data.title}</h1>
+                        <p>Produced by <span class="text-lg font-bold">${prod.length > 0 ? prod[0].name : `Unknown`}</span></p>
+                        <p class="my-8 leading-relaxed">${data.overview}</p>
+                        <div class="rating__date">
+                            <p class="w-16 h-8 bg-red-400 text-white inline-block">Rating</p>
+                            <p class="inline-block">${data.release_date}</p>
+                        </div>
+                        <div class="genres">
+                            <p class="inline-block w-20 h-12">${genre[0].name}</p>
+                        </div>
+                        <button>More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    document.getElementsByClassName('modal__film')[0].insertAdjacentHTML('afterbegin', html);
+    document.getElementsByClassName('modal__film')[0].classList.remove('hidden');
+
+}
+
 function renderNowPlaying(data) {
-    const html = `<div onclick="alert('Film ${data.title} sedang berlangsung')" class="component m-4">
+    const html = `<div onclick="specificMovie('${data.id}')" class="component m-4 hover:shadow-2xl hover:animate-ping">
             <img class="w-32" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}" alt="">
             <p class="text-base bg-red-600 text-white text-center">Now Playing</p>
             <h1 class="text-lg font-bold w-32">${data.title}</h1>
@@ -12,7 +51,7 @@ function renderNowPlaying(data) {
 } 
 
 function renderUpcoming(data) {
-    const html = `<div onclick="alert('Film ${data.title} akan segera tayang')" class="component m-4">
+    const html = `<div onclick="specificMovie('${data.id}')" class="component m-4 hover:shadow-2xl hover:animate-ping">
             <img class="w-32" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}" alt="">
             <p class="text-base bg-blue-600 text-white text-center">Upcoming</p>
             <h1 class="text-lg font-bold w-32">${data.title}</h1>
@@ -20,6 +59,7 @@ function renderUpcoming(data) {
             <p class="text-xs text-gray-300 w-32">${data.release_date}</p>
         </div>
     `
+    // console.log(data.toString())
     return html;
 }
 
@@ -44,7 +84,7 @@ async function nowPlaying() {
  nowPlaying();
 
 const renderInfo = (data) => {
-    const html = `<div class="component m-4">
+    const html = `<div onclick="specificMovie('${data.id}')" class="component m-4 hover:shadow-2xl hover:animate-ping">
             <img class="w-32" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}" alt="">
             <h1 class="text-lg font-bold w-32">${data.title}</h1>
             <p class="w-32">${data.vote_average === 0 ? 'Unrated' : data.vote_average}</p>
@@ -66,15 +106,11 @@ async function reqApiSearch(query) {
     }
 }
 
-// async function reqApiMovie(query) {
-//     try {
-//         let data = await fetch(`https://api.themoviedb.org/3/movie/207936?api_key=2eb8dae5e1988df3f72f80c01551e09a&language=en-US`);
-//         data = await data.json();
-//         console.log(data)
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
+async function specificMovie(id) {
+    let data = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=2eb8dae5e1988df3f72f80c01551e09a&language=en-US`);
+    data = await data.json();
+    showModal(data)
+}
 
 function searchMovies(query) {
     const queryRegex = /\s+/gm;
